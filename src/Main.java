e7dda87ce6f1c23d8a99b25dff2b5f36de63889d
SergKry <necroastral@mail.ru>
2022-05-21 22:25:41 +0400

    ДЗ1 задание 2

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amout = -180021; // сумма билета
+        int formyla = amout / 20 * 1; // формула расчета бонусных баллов
+        int bonus = formyla;
+        System.out.println("Итого бонусов за покупку " +bonus);
+
+        
+    }
+}
